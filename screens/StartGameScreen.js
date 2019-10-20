4f2f8b5222e8f7601d39795cd83688aec38eff86
Stephan <sgriesel@gmail.com>
2019-10-20 05:24:32 +0200

    style input container in card style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,15 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: '#FFFFFF',
+        elevation: 7,
+        padding: 20,
+        borderRadius: 15
     },
     buttonContainer: {
         flexDirection: 'row',
