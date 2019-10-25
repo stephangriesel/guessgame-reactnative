07b43f2908442b0c10582592f36f7ffc5f333f04
Stephan <sgriesel@gmail.com>
2019-10-25 06:38:53 +0200

    add logic for confirm & reset

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -15,10 +15,33 @@ import Input from '../components/Input';
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState();
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
+
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
 
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+    }
     return (
         <TouchableWithoutFeedback onPress={() => {
             Keyboard.dismiss();
@@ -37,10 +60,21 @@ const StartGameScreen = props => {
                         value={enteredValue}
                     />
                     <View style={styles.buttonContainer}>
-                        <View style={styles.singleButton}><Button title="Confirm" onPress={() => { }} color={Colors.primary} /></View>
-                        <View style={styles.singleButton}><Button title="Reset" onPress={() => { }} color={Colors.secondary} /></View>
+                        <View style={styles.singleButton}>
+                            <Button
+                                title="Confirm"
+                                onPress={confirmInputHandler}
+                                color={Colors.primary} />
+                        </View>
+                        <View style={styles.singleButton}>
+                            <Button
+                                title="Reset"
+                                onPress={resetInputHandler}
+                                color={Colors.secondary} />
+                        </View>
                     </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     );
