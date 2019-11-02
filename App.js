066a366abf98d2fcb11a97f407b4fdec803b1661
Stephan <sgriesel@gmail.com>
2019-11-02 08:30:22 +0100

    add logic for win condition

--- App.js
+++ App.js
@@ -3,18 +3,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameoverScreen from './screens/GameoverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numberOfRounds => {
+    setGuessRounds(numberOfRounds);
   };
 
   // Get selected number from startGameScreen when start button pressed, then store it in state of app component (selectedNumber)  then passed down to GameScreen if condition met. In GameScreen userNumber is passed with userChoice prop & generates new number which is opponent guess
   let content = <StartGameScreen onStartGame={startGameHandler} />
 
-  if (userNumber) { // if userNumber is true
-    content = <GameScreen userChoice={userNumber} />; // display content GameScreen
+  if (userNumber && guessRounds <= 0) { // if userNumber is true
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />; // display content GameScreen
+  } else if (guessRounds > 0) {
+    content = <GameoverScreen />
   }
 
   return (
